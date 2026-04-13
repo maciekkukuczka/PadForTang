10b3f85c64fd816aa6573840976aec886e89e7a0
mac <maciek@kukuczka.net>
2026-04-13 22:14:53 +0200

    Refactor arcade stick setup and input handling; improve pin configuration and add debug output

--- include/Config.h
+++ include/Config.h
@@ -1,3 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <Arduino.h>
+
 // Pad
 #define USE_XBOX_PAD 1
 #define XBOX_CONTROLLER_MAC "3c:fa:06:76:00:51"
@@ -5,4 +9,5 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
 
-#define MAC_ADDRESS_BASE "ac:eb:e6:1a:75:90"
+const uint8_t MAC_ADDRESS_BASE[] = {0xac, 0xeb, 0xe6, 0x1a, 0x75, 0x90};
+#endif
--- src/arcade_stick.cpp
+++ src/arcade_stick.cpp
@@ -1,12 +1,10 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <esp_now.h>
-#include "Config.h"
+#include "Config.h" 
 
-typedef struct struct_message
-{
+typedef struct struct_message {
     uint8_t knob;
-
     bool a;
     bool b;
     bool x;
@@ -15,66 +13,86 @@ typedef struct struct_message
     bool r;
     bool start;
     bool select;
-
 } struct_message;
 
 struct_message data;
 
-void setup()
-{
+void sendData(); 
+void printDebug(); 
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Uruchamianie Arcade Sticka...");
+
     WiFi.mode(WIFI_STA);
-    if (!esp_now_init())
-        return;
+    if (!esp_now_init()) {
+        Serial.println("Blad: Nie mozna uruchomic ESP-NOW!");
+        return; 
+    }
+
     esp_now_peer_info_t info;
-    info.channel=0;
-    info.encrypt=false;
+    info.channel = 0;
+    info.encrypt = false;
     memcpy(info.peer_addr, MAC_ADDRESS_BASE, 6);
     esp_now_add_peer(&info);
 
-    pinMode(0, INPUT);
-    pinMode(1, INPUT);
+    // Gałka
+    pinMode(2, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);
+    pinMode(5, INPUT_PULLUP);
 
+    // Start, Select, Bumpery
+    pinMode(0, INPUT_PULLUP);
+    pinMode(1, INPUT_PULLUP);
+    pinMode(6, INPUT_PULLUP);
+    pinMode(7, INPUT_PULLUP);
 
+    // POPRAWIONE PINY A, B, X, Y (Muszą się zgadzać z odczytem!)
+    pinMode(20, INPUT_PULLUP); // A
+    pinMode(16, INPUT_PULLUP); // B  (TX)
+    pinMode(17, INPUT_PULLUP); // X  (RX)
+    pinMode(14, INPUT_PULLUP); // Y
+
+    Serial.println("Gotowy do walki!");
 }
 
-void loop()
-{
-    send(data);
-    delay(10);
+void loop() {
+    sendData();   
+    printDebug(); 
+    delay(50);    
 }
 
-void send(struct_message data)
-{
-    if (digitalRead(2))
-    {
-        data.knob = 0b00000001;
-    }
-    else if (digitalRead(3))
-    {
-        data.knob = 0b00000010;
-    }
-    else if (digitalRead(4))
-    {
-        data.knob = 0b00000100;
-    }
-    else if (digitalRead(5))
-    {
-        data.knob = 0b00001000;
-    }
-    else
-    {
-        data.knob = 0b00000000;
-    }
+void sendData() {
+    // 1. ZEROWANIE GAŁKI (Na starcie cyklu postać stoi w miejscu)
+    data.knob = 0b00000000;
 
-    data.start = digitalRead(0);
-    data.select = digitalRead(1);
+    // 2. ODCZYT KIERUNKÓW (Używamy |= żeby móc dodawać bity - SKOSY!)
+    if (!digitalRead(2)) data.knob |= 0b00000001; // Góra
+    if (!digitalRead(3)) data.knob |= 0b00000010; // Dół
+    if (!digitalRead(4)) data.knob |= 0b00000100; // Lewo
+    if (!digitalRead(5)) data.knob |= 0b00001000; // Prawo
 
-    data.a = digitalRead(6);
-    data.b = digitalRead(7);
-    data.x = digitalRead(10);
-    data.y = digitalRead(11);
-    data.l = digitalRead(12);
-    data.r = digitalRead(13);
+    // 3. ODCZYT PRZYCISKÓW (Z Twoimi nowymi pinami)
+    data.start = !digitalRead(0);
+    data.select = !digitalRead(1);
 
+    data.a = !digitalRead(20);
+    data.b = !digitalRead(16);
+    data.x = !digitalRead(17);
+    data.y = !digitalRead(14);
+
+    data.l = !digitalRead(6);
+    data.r = !digitalRead(7);
+
+    // 4. WYSŁKA
     esp_now_send(MAC_ADDRESS_BASE, (uint8_t *)&data, sizeof(data));
-};
+}
+
+void printDebug() {
+    Serial.printf("Galka: %08b | A:%d B:%d X:%d Y:%d | L:%d R:%d | Start:%d Select:%d\n",
+                  data.knob, 
+                  data.a, data.b, data.x, data.y, 
+                  data.l, data.r, 
+                  data.start, data.select);
+}
